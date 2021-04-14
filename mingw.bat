e6bcf3f61435290c320591142b4c6241039344f2
stophin <kittybella@163.com>
2021-04-14 20:17:00 +0800

    memory check utils

--- mingw.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set MINGW=C:\MinGW
-set PATH=%PATH%;%MINGW%\bin;%MINGW%\mingw32\bin\;%MINGW%\msys\1.0\bin;D:\cmake-3.18.0-rc1-win64-x64\bin\
-
-set LIBRARY_PATH=F:\xege.git\lib\mingw\lib
-set CPLUS_INCLUDE_PATH=F:\xege.git\src
-
-cmd
